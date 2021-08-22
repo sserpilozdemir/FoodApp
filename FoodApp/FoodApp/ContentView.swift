a7903ad5c661eb5aea664f73bc33a9f69ca92040
sserpilozdemir <serpil.ozdemir@windowslive.com>
2021-08-22 21:27:40 +0300

    firstcommit

---
+++ FoodApp/FoodApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodApp
+//
+//  Created by Cemalettin Yılmaz on 22.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodApp/FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Cemalettin Yılmaz on 22.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
